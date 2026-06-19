01cdc3838a116c406afc27ded09a42f9368e5a1c
attarnia <siralfa2@gmail.com>
2026-06-19 16:43:29 +0330

    bug fixed

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -66,7 +66,6 @@ export default function Navbar() {
               </div>
             </div>
 
-<<<<<<< HEAD
             {open && (
               <nav
                 className="md:hidden mt-3 pt-3 border-t border-black/5 dark:border-white/10"
@@ -89,37 +88,6 @@ export default function Navbar() {
                       href="/signup"
                       className="block text-center bg-black text-white dark:bg-white dark:text-black py-2 rounded-full text-sm"
                       onClick={() => setOpen(false)}
-=======
-            <svg className="absolute w-0 h-0 invisible" aria-hidden="true">
-                <filter
-                    id="glass-distortion"
-                    x="0%"
-                    y="0%"
-                    width="100%"
-                    height="100%"
-                    filterUnits="objectBoundingBox"
-                >
-                    <feTurbulence
-                        type="fractalNoise"
-                        baseFrequency="0.01 0.01"
-                        numOctaves="1"
-                        seed="5"
-                        result="turbulence"
-                    />
-                    <feComponentTransfer in="turbulence" result="mapped">
-                        <feFuncR type="gamma" amplitude="1" exponent="10" offset="0.5" />
-                        <feFuncG type="gamma" amplitude="0" exponent="1" offset="0" />
-                        <feFuncB type="gamma" amplitude="0" exponent="1" offset="0.5" />
-                    </feComponentTransfer>
-                    <feGaussianBlur in="turbulence" stdDeviation="1" result="softMap" />
-                    <feSpecularLighting
-                        in="softMap"
-                        surfaceScale="5"
-                        specularConstant="1"
-                        specularExponent="100"
-                        lightingColor="white"
-                        result="specLight"
->>>>>>> 288903eea5137d280acea615eb5f0b22396ea6b0
                     >
                       Get Started
                     </Link>
